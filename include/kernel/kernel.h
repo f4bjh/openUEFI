56552c07a71335ab1f119bd831e00e5eb6268977
f4bjh <f4bjh_95@yahoo.fr>
2026-02-28 11:13:38 +0100

    32 - [include] introduce kernel state and its control

--- include/kernel/kernel.h
+++ include/kernel/kernel.h
@@ -14,20 +14,36 @@
 #define BIT1 0x02
 #define BIT0 0x01
 
+typedef enum {
+ MUKERNEL_NOT_INITIALISED,
+ MUKERNEL_INITIALISED,
+ MUKERNEL_INTERFACE_REGISTERED,
+ MUKERNEL_STARTED,
+ MUKERNEL_STOPPED
+} mukernel_state_t;
+
+typedef enum {
+    MUKERNEL_OK = 0,
+    MUKERNEL_ERR_INVALID_STATE,
+    MUKERNEL_ERR_ALREADY_REGISTERED,
+    MUKERNEL_ERR_NOT_INITIALISED,
+    MUKERNEL_ERR_INTERNAL
+} mukernel_err_status_t;
+
 typedef uint8_t errno_t;
 typedef  errno_t (f_init_t)(void); //faut il envisager de passer un contexte aux fonctions d'init
 				//exemple : au shim, les fonctions d'appel d'interface a get_time etc etc
 				//et a terme idem au bootloader sentinel (typiquement, system_table ?
 
 typedef struct {
+  mukernel_state_t state;
   f_init_t *f_init;
 } mukernel_t;
 
 void _start(void); 
-void mukernel_init(void);
-void mukernel_start(void);
+mukernel_err_status_t mukernel_init(void);
+mukernel_err_status_t mukernel_start(void);
 errno_t mukernel_register_interface(f_init_t*);
-
-
+void mukernel_panic(void);
 
 #endif
